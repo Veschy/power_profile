6230853953151c906f30b71c9e573153632d060d
Гнеушев Олег <o.gneushev@TITANPS.RU>
2024-02-13 13:01:59 +0300

    квадратный график

--- main.py
+++ main.py
@@ -2,24 +2,33 @@ import pandas as pd
 
 data_set = pd.read_csv('time_power_set.csv', delimiter=';', header=0)
 set_list = list(data_set['Power'].tolist())
-# print(set_list)
 sum =0
 for b in set_list:
-    sum  += b
-avg = round(sum/len(set_list))
-rez = [0]
-n, k = 0, 0
-for t in range(len(set_list)):
-    if set_list[t] < avg:
-        k += set_list[t]-avg
-        print(' меньше ', t, k )
+    sum += b
+avg = round(sum/(len(set_list)-1))
+print(avg)
+rez = []
+time_less, time_more, power_less, power_more = 0, 0, 0, 0
+time = 0
+for t1 in range(len(set_list)):
+    if set_list[t1] < avg:
+        if time_more > 0:
+            rez.append([time_more, power_more, round(power_more / time_more)])
+        power_more = 0
+        time_more = 0
+        power_less += set_list[t1] - avg
+        time_less += 1
     else:
-        n += set_list[t]-avg
-        print(' больше ', t, n)
-
-#         print(rez)
-# print(k)
-
+        if time_less > 0:
+            rez.append([time_less,power_less, round(power_less/time_less)])
+        power_less = 0
+        time_less = 0
+        power_more += set_list[t1] - avg
+        time_more += 1
+for t2 in range(len(rez)):
+    print('длительность / энергия / средняя мощность ', rez[t2])
+print('Среднее значение  мощности за всю выборку =', avg)
+print('Количетсво интервалов', len(rez))
 
 
 
